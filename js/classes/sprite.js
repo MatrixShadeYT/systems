495bcaeeb8b52aa7b57477c7e8c40afaabe5fa4e
MatrixShadeYT <mpro9234@gmail.com>
2026-01-09 23:43:10 -0700

    v0.01

---
+++ js/classes/sprite.js
@@ -0,0 +1,43 @@
+class Sprite {
+    constructor({position, imageSrc, frameRate=1, frameBuffer=0, scale=1}) {
+        this.frameRate = frameRate;
+        this.position = position;
+        this.image = new Image();
+        this.image.onload = () => {
+            this.loaded = true;
+            this.width = this.image.width/this.frameRate;
+            this.height = this.image.height;
+        }
+        this.image.src = imageSrc;
+        this.frameBuffer = frameBuffer;
+        this.ellapsedFrames = 0;
+        this.currentFrame = 0;
+        this.loaded = false;
+        this.scale = scale;
+    }
+    draw(ctx) {
+        if (!this.loaded) return;
+        ctx.drawImage(
+            this.image,
+            this.currentFrame*16,
+            0,
+            this.width,
+            this.height,
+            this.position.x,
+            this.position.y,
+            this.width*this.scale,
+            this.height*this.scale
+        );
+        this.updateFrame();
+    }
+    updateFrame() {
+        this.ellapsedFrames++;
+        if (this.ellapsedFrames % this.frameBuffer === 0) {
+            if (this.currentFrame < this.frameRate - 1) {
+                this.currentFrame++
+            } else {
+                this.currentFrame = 0;
+            }
+        }
+    }
+}
